bbd57f00e0e6c3dd79ac9f939040a72861adaba5
Cristopher <crisesv4@gmail.com>
2025-08-19 23:05:49 -0600

    Create read_CO2.py

---
+++ api/Devices/read_CO2.py
@@ -0,0 +1,13 @@
+from ..index import app, devicescollection
+from flask import request, jsonify
+
+@app.route("/api/Devices/read-CO2", methods=["POST"])
+def read_CO2():
+    data = request.get_json()
+    email = data.get("email")
+    team_code = data.get("team_code")
+    if not email and not team_code:
+        return jsonify({"error": "Email or team_code required"}), 400
+    devices = list(devicescollection.find({"email": email} if email else {"team": team_code}, {"_id": False}))
+    total_CO2, max_device_info = CalculateCO2(devices)
+    return jsonify({"total_CO2": total_CO2, "device_mas_CO2": max_device_info}), 200
